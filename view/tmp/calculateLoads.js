ef879f88ba6d99fb1460f69943423a38a80b5119
Michael Gerov <mhgerov@gmail.com>
2018-05-22 12:08:10 -0400

    function finished

--- view/tmp/calculateLoads.js
+++ view/tmp/calculateLoads.js
@@ -22,3 +22,35 @@ const project = {
   ]
 }
 
+function calculateLoads(project) {
+	let genLoad = [0,0,0];
+	for (i in project.loads) {
+		let ampsAdded = [0,0,0];
+		let load = project.loads[i];
+		if (load.phase=='single') {
+			switch (load.connection) {
+				case('L1'):
+					ampsAdded[0]=load.current;
+					break;
+				case('L2'):
+					ampsAdded[1]=load.current;
+					break;
+				case('L3'):
+					ampsAdded[2]=load.current;
+					break;
+			}
+		} else { //it's 3-phase
+			ampsAdded = [load.current/3,load.current/3,load.current/3];
+		}	
+		//Multiply by PF constant if inductive load
+		if (load.type=='inductive') {
+			ampsAdded = ampsAdded.map(x => 0.8*x);
+		}
+		for (i in genLoad) {
+			genLoad[i]+=ampsAdded[i];
+		}
+	}
+	return genLoad;
+}
+
+console.log(calculateLoads(project));
