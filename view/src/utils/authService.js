ef9c2de7c9467ff172760b66367730836ba52ca6
Will Miller <willmiller@Wills-MacBook-Pro.local>
2018-05-22 10:09:03 -0400

    dummy auth for testing

---
+++ view/src/utils/authService.js
@@ -0,0 +1,38 @@
+// all of the authentication logic lives here. so any functions that you will need
+// anywhere in the app will live here and be imported to the files that they are needed in.
+// you will need to import two things here. 
+    // 1. Decode from 'jwt-decode'
+    // 2. Auth0 from 'auth0-js'
+
+// consts
+
+
+// functions
+// requireAuthorization
+
+// login
+// obviously login functionality
+
+// logout
+// obviously logout functionality
+
+// signup
+// obviously signup functionality
+
+// getIdToken
+// fetches the id token for the user as they are logging in
+
+// clearIdToken
+// clears the id token
+
+// setIdToken
+
+// getAccessToken
+
+// clearAccessToken
+
+// setAccessToken
+
+// isLoggedin
+
+// isTokenExpired
