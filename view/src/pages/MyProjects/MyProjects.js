81377a089b2a253906a514bda1c7ba981260a2cb
Orit Freud <oritfreud@Orits-MacBook-Air.local>
2018-05-29 11:15:17 -0400

    crud working

--- view/src/pages/MyProjects/MyProjects.js
+++ view/src/pages/MyProjects/MyProjects.js
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import '../../components/Auth/Auth';
 import { setIdToken, setAccessToken, isLoggedIn } from "../../components/Auth/Auth";
 import jwt_decode from 'jwt-decode';
+import Footer from "../../components/Footer";
 
 
 
@@ -76,9 +77,11 @@ class MyProjects extends Component {
                         </Row>
                     </div>
                 </Container>
+                <Footer />
             </div>
         )
     }        
 }
+
 
 export default MyProjects;
