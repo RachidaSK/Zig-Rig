64ae369d720fdb140db59722c8430b92f68d1abf
Caroline Rink <carolinerink@MacBook-Pro-2.local>
2018-05-30 13:10:16 -0400

    css fixes to modal and load

--- view/src/components/BuiltLoadComponent/BuiltLoadComponent.js
+++ view/src/components/BuiltLoadComponent/BuiltLoadComponent.js
@@ -6,41 +6,57 @@ import "./BuiltLoadComponent.css";
 const BuiltLoadComponent = (props) => (
     <Container fluid>
         <Row>
-            <Column className="builtComponentColumn" size="lg-8">
+            <Column className="builtComponentColumn" size="lg-12">
                 <div className="builtComponentDiv">
                     <Row>
-                        <Column size="lg-12">
+                        <Column size="lg-12 md-12 sm-12">
                             <div className="savedLoadTitle">
-                                Load Name: {props.loadData.name}
+                            <Column size="lg-12 md-12 sm-12">
+                                <b>Load Name:</b> 
+                            </Column> 
+                            <Column size="lg-12 md-12 sm-12">
+                                {props.loadData.name}
+                            </Column>
                             </div>
                         </Column>
                     </Row>
                     <br />
                     <Row>
-                        <Column size="lg-4">
+                        <Column size="lg-4 md-12 sm-12">
                             <div className="savedProjectPhase">
-                                Phase: {props.loadData.phase}
+                                <Column size="lg-4 md-12 sm-12">
+                                    <b>Phase:</b> {props.loadData.phase}
+                                </Column>
                             </div>
                         </Column>
-                        <Column size="lg-4">
+                        <Column size="lg-4 md-12 sm-12">
                             <div className="savedProjectType">
-                                Type: {props.loadData.type}
+                                <Column size="lg-4 md-12 sm-12">
+                                    <b>Type:</b> {props.loadData.type}
+                                </Column>
                             </div>
                         </Column>
-                        <Column size="lg-4">
+                        <Column size="lg-4 md-12 sm-12">
                             <div className="savedProjectCurrent">
-                                Current: {props.loadData.current*1}
+                                <Column size="lg-4 md-12 sm-12">
+                                    <b>Current:</b> {props.loadData.current*1}
+                                </Column>
                             </div>
                         </Column>
                     </Row>
                     <br />
                     <Row>
-                        <Column size="lg-9">
+                        <Column size="lg-10">
                             <div className="savedProjectLegs">
-                                Legs: L1: {Object.values(props.loadData.connections[0])}  L2: {Object.values(props.loadData.connections[1])}  L3: {Object.values(props.loadData.connections[2])} 
+                                <Column size="lg-12 md-12 sm-12">
+                                    <b>Legs:</b>
+                                </Column>
+                                <Column size="lg-12 md-12 sm-12">
+                                    L1: {Object.values(props.loadData.connections[0])}  L2: {Object.values(props.loadData.connections[1])}  L3: {Object.values(props.loadData.connections[2])} 
+                                </Column>
                             </div>
                         </Column>
-                        <Column size="lg-3">
+                        <Column size="lg-2">
                             <div className="editLoadButton">
                                 <EditButton />
                             </div>
--- view/src/components/FormModal/FormModal.js
+++ view/src/components/FormModal/FormModal.js
@@ -80,101 +80,117 @@ class FormModal extends React.Component {
         return (
             <div>
                 <Row>
-                    <InputModal
-                        value={this.name}
-                        onChange={this.handleInputModalChange}
-                        name="name"
-                        placeholder="New Draw Title"
-                    />
+                    <Column size="lg-12">
+                        <InputModal
+                            value={this.name}
+                            onChange={this.handleInputModalChange}
+                            name="name"
+                            placeholder="New Draw Title"
+                        />
+                    </Column>
                 </Row>
                 <br />
                 <Row>
                     <Column size="lg-4">
                         <Row>
-                            <label id="phase1">
-                                Phase:
-                            </label>
+                            <Column size="lg-12 md-12 sm-12">
+                                <label id="phase1">
+                                    Phase:
+                                </label>
+                            </Column>
                         </Row>
                         <Row>
-                            <label className="checkbox">
-                                <input type="radio" className="phase2 checkbox-control" checked={this.state.phase === 'Single Phase'} value="Single Phase" onChange={this.switchPhase}/>
-                                <span className="checkbox-label">Single Phase</span>
-                            </label>
+                            <Column size="lg-12 md-12 sm-12">
+                                <label className="checkbox">
+                                    <input type="radio" className="phase2 checkbox-control" checked={this.state.phase === 'Single Phase'} value="Single Phase" onChange={this.switchPhase}/>
+                                    <span className="checkbox-label"> Single Phase</span>
+                                </label>
+                            </Column>
                         </Row>
                         <Row>
-                            <label className="checkbox">
-                                <input type="radio" className="phase2 checkbox-control" checked={this.state.phase === 'Three Phase'} value="Three Phase" onChange={this.switchPhase}/>
-                                <span className="checkbox-label">Three Phase</span>
-                            </label>
+                            <Column size="lg-12 md-12 sm-12">
+                                <label className="checkbox">
+                                    <input type="radio" className="phase2 checkbox-control" checked={this.state.phase === 'Three Phase'} value="Three Phase" onChange={this.switchPhase}/>
+                                    <span className="checkbox-label"> Three Phase</span>
+                                </label>
+                            </Column>
                         </Row>
                     </Column>
                     <Column size="lg-4">
                         <Row>
-                            <label id="type1">
-                                Type:
-                            </label>
+                            <Column size="lg-12 md-12 sm-12">
+                                <label id="type1">
+                                    Type:
+                                </label>
+                            </Column>
                         </Row>
                         <Row>
-                            <label className="checkbox">
-                                <input type="radio" className="type2 checkbox-control" checked={this.state.type === 'Resistive'} value="Resistive" onChange={this.switchType}/>
-                                <span className="checkbox-label3">Resistive</span>
-                            </label>
+                            <Column size="lg-12 md-12 sm-12">
+                                <label className="checkbox">
+                                    <input type="radio" className="type2 checkbox-control" checked={this.state.type === 'Resistive'} value="Resistive" onChange={this.switchType} />
+                                    <span className="checkbox-label3"> Resistive</span>
+                                </label>
+                            </Column>
                         </Row>
                         <Row>
-                        <label className="checkbox">
-                            <input type="radio" className="type2 checkbox-control" checked={this.state.type === 'Inductive'} value="Inductive" onChange={this.switchType}/>
-                            <span className="checkbox-label">Inductive</span>
+                            <Column size="lg-12 md-12 sm-12">
+                                <label className="checkbox">
+                                    <input type="radio" className="type2 checkbox-control" checked={this.state.type === 'Inductive'} value="Inductive" onChange={this.switchType} />
+                                    <span className="checkbox-label"> Inductive</span>
+                                </label>
+                            </Column>
+                        </Row>
+                    </Column>
+                    <Column size="lg-4">
+                        <Row>
+                            <Column size="lg-12">
+                                <label id="current">
+                                    Current:
+                                </label>
+                            </Column>
+                        </Row>
+                        <Row>
+                            <Column size="lg-12">
+                                <InputModal
+                                    value={this.current}
+                                    onChange={this.handleInputChange}
+                                    name="current"
+                                    placeholder="0"
+                                />
+                            </Column>
+                        </Row>
+                    </Column>
+                </Row>
+                <br />
+                <Row>
+                    <Column size="lg-12 md-12 sm-12">
+                        <label id="selectLegsLabel">
+                            Select Legs:
                         </label>
-                        </Row>
                     </Column>
-                    <Column size="lg-4">
-                        <Row>
-                            <label id="current">
-                                Current:
-                            </label>
-                        </Row>
-                        <Row>
-                            <InputModal
-                                value={this.current}
-                                onChange={this.handleInputChange}
-                                name="current"
-                                placeholder="0"
-                            />
-                        </Row>
-                    </Column>
-                </Row>
-                <br />
-                <Row>
-                    <label id="selectLegsLabel">
-                        Select Legs:
-                    </label>
                     <br />
-                    <Column size="lg-2">
-                    <label className="checkbox">
+                    <Column size="lg-2 md-4 sm-12">
+                        <label className="checkbox">
                             <input id="L1" type="radio" name="L1" className="checkbox-control" checked={this.state.phase === "Three Phase" || this.state.legs.L1} value="L1" onChange={this.switchConnection}/>
                             <span className="checkbox-label">L1</span>
                         </label>
                     </Column>
-                    <Column size="lg-2">
-                    <label className="checkbox">
+                    <Column size="lg-2 md-4 sm-12">
+                        <label className="checkbox">
                             <input id="L2" type="radio" name="L2" className="checkbox-control" checked={this.state.phase === "Three Phase" || this.state.legs.L2} value="L2" onChange={this.switchConnection}/>
                             <span className="checkbox-label">L2</span>
                         </label>
                     </Column>
-                    <label className="checkbox">
+                    <Column size="lg-4 md-4 sm-12">
+                        <label className="checkbox">
                             <input id="L3" type="radio" name="L3" className="checkbox-control" checked={this.state.phase === "Three Phase" || this.state.legs.L3} value="L3" onChange={this.switchConnection}/>
                             <span className="checkbox-label">L3</span>
                         </label>
-                    <Column size="lg-2">
+                    </Column>
+                    <Column size="lg-4 md-12 sm-12">
+                        <AddButtonModal onClick={this.handleFormSubmit}/>
                     </Column>
                 </Row> 
-             <br />  
-                <br />     
-                <Row >
-                    <b className="currentLegTotalsModal">Current Leg Totals:</b><br/>
-                    <b className="currentLegTotalsModal">L1:</b> <i>0</i>  <b>L2:</b> <i>0</i>  <b>L3:</b> <i>0</i>  <b>N:</b> <i>0</i>
-                    <AddButtonModal onClick={this.handleFormSubmit}/>
-                </Row>
             </div>
 
         )
